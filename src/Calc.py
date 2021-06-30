f6a7dbe60233585c3cb16d8c8a2a9b4c91d1dfd0
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-06-29 20:18:50 -0500

    Feature: Created new branch and Added Addition function and merge into main branch

--- src/Calc.py
+++ src/Calc.py
@@ -1,5 +1,13 @@
+def addition(a, b):
+    return a + b
+
+
 class Calc:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
--- src/CalcUnitTest.py
+++ src/CalcUnitTest.py
@@ -6,3 +6,11 @@ class MyUnitTest(unittest.TestCase):
     def test_instantiate_calculator(self):
         calc = Calc()
         self.assertIsInstance(calc, Calc)
+
+    def test_add_method_Calc(self):
+        calc = Calc()
+        self.assertEqual(calc.add(2, 2), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
