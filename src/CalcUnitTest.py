889c147ef9b5e52f336a801eddc785fa97302828
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-06-30 20:38:58 -0500

    Feature: Added Division csv file

--- src/CalcUnitTest.py
+++ src/CalcUnitTest.py
@@ -13,19 +13,19 @@ class MyUnitTest(unittest.TestCase):
         self.assertIsInstance(self.calc, Calc)
 
     def test_addition(self):
-        test_add_data = CSVReader("/src/Unit_Test_Addition.csv").data
+        test_add_data = CSVReader("/src/CSVFiles/Unit_Test_Addition.csv").data
         for row in test_add_data:
             self.assertEqual(self.calc.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calc.result, int(row['Result']))
 
     def test_subtraction(self):
-        test_sub_data = CSVReader("/src/Unit_Test_Subtraction.csv").data
+        test_sub_data = CSVReader("/src/CSVFiles/Unit_Test_Subtraction.csv").data
         for row in test_sub_data:
             self.assertEqual(self.calc.sub(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calc.result, int(row['Result']))
 
     def test_multiplication(self):
-        test_multiple_data = CSVReader("/src/Unit_Test_Multiplication.csv").data
+        test_multiple_data = CSVReader("/src/CSVFiles/Unit_Test_Multiplication.csv").data
         self.assertEqual(self.calc.sub(5, 10), 5)
         for row in test_multiple_data:
             self.assertEqual(self.calc.multiple(row['Value 1'], row['Value 2']), int(row['Result']))
