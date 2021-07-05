dbb67f3701960a1265364b504267c9269a605535
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-05 10:41:24 -0500

    Feature: Added New Static variable file for Static text use in code

---
+++ src/StaticVariable.py
@@ -0,0 +1,10 @@
+class StaticVariable:
+    unitTestAddition = "src/CSVFiles/Unit_Test_Addition.csv"
+    unitTestSubtraction = "/src/CSVFiles/Unit_Test_Subtraction.csv"
+    unitTestMultiplication = "/src/CSVFiles/Unit_Test_Multiplication.csv"
+    unitTestDivision = "/src/CSVFiles/Unit_Test_Division.csv"
+    unitTestSquare = "/src/CSVFiles/Unit_Test_Square.csv"
+    unitTestSquareRoot = "/src/CSVFiles/Unit_Test_SquareRoot.csv"
+    value1 = "Value 1"
+    value2 = "Value 2"
+    result = "Result"
